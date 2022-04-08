8a469e535f5b5f8086d1cdfc47cd808ad3dccea6
zjcrumpton <zjcrumpton@gmail.com>
2022-04-08 15:25:56 -0600

    render main character to the screen

--- main.cpp
+++ main.cpp
@@ -1,11 +1,55 @@
 #include <ncurses.h>
 
-int main() {
+// Main character initial position and symbol
+int row = 10, col = 10;
+const char main_char = '@';
+
+// Initialize ncurses
+void initScreen() {
+    // init ncurses data structures and allocate memory
     initscr();
+
+    /**
+     * Clear the internal state of the screen.
+     * Does not clear the visual screen until refresh is called.
+     */
     clear();
 
-    printw("Seems you can use ncurses... \nPress any key to exit");
-    getch();
+    // prevents user input from being echoed
+    noecho();
 
-    endwin();
+    // disable line buffering
+    cbreak();
+
+    // enable the keypad of the user's terminal
+    keypad(stdscr, TRUE);
+
+    // make the cursor invisible
+    curs_set(0);
+}
+
+void welcomeMessage() {
+    printw("Welcome to Rogue.\nPress any key to start.\nIf you want to quit, press \"Q\" or \"q\"");
+}
+
+int main() {
+    initScreen();
+    welcomeMessage();
+    
+    // wait until the user presses a key
+    int ch = getch();
+
+    clear();
+
+    // main game loop
+    while(1) {
+        // quit game if user presses Q
+        if (ch == 'q' || ch == 'Q') {
+            endwin();
+            break;
+        } else {
+            mvaddch(row, col, main_char);
+            ch = getch();
+        }
+    }
 }
